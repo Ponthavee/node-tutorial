60b9fd0d6b4f32bbcdca6a313b28398f11e5d5b7
Ponthavee <ponthavee@hotmail.com>
2018-10-01 01:00:24 +0700

    git commit -m "first commit"

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.json({res: 'hello world'});
+});
+
+app.get('/show', function(req, res){
+    res.json({res: 'show show'});
+});
+
+var server = app.listen(3000, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("listenting at http://%s:%s".host, port);
+});
