48bd694059d334c9728c6dd3c20de411731d9768
Ponthavee <ponthavee@hotmail.com>
2018-10-01 01:05:37 +0700

    git push -u origin master

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('/show', function(req, res){
 var server = app.listen(3000, function(){
     var host = server.address().address;
     var port = server.address().port;
-    console.log("listenting at http://%s:%s".host, port);
+    console.log("listenting at http://%s:%s",host, port);
 });
